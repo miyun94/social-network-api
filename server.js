b091a7ffd0bd87d07fb398f3ffd2baf016d6e05b
echo <miyunniecho@gmail.com>
2021-04-15 23:11:05 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const db = require('./Models'); 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/populatedb', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('debug', true);
